80af66af602674357bee15a5f1140b90117b9bce
José Miguel <Tapuy@hotmail.es>
2024-06-27 13:26:31 +0200

    react router y spinner de texto añadido

--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 import NewPost from "./NewPost";
@@ -6,10 +6,31 @@ import Modal from "./Modal";
 
 function PostsList({ modalIsVisible, modalNotVisible }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const data = await response.json();
+      setPosts(data.posts);
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
+
+  // se establece primero el nuevo mensaje, seguido de una copia de los anteriores mensajes
+  // porque se ha usado el spread operator
+  // fetch() sirve para traer datos y para enviar datos también
   function addPostHandler(postData) {
-    // se establece primero el nuevo mensaje, seguido de una copia de los anteriores mensajes
-    // porque se ha usado el spread operator
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
     setPosts((existingPost) => [postData, ...existingPost]);
   }
 
@@ -22,17 +43,22 @@ function PostsList({ modalIsVisible, modalNotVisible }) {
           <NewPost onCancel={modalNotVisible} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={`${styles["posts"]}`}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
